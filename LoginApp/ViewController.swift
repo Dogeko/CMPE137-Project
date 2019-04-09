9e12493eb153338808baf9cdbe9fd9906bb86252
Robin Chou <robinchou@robins-macbook-pro.local>
2019-04-09 05:32:12 -0700

    Initial Commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Robin Chou on 4/9/19.
+//  Copyright © 2019 Robin Chou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
