a54e451a5332e287c09d5a2fe35751de8ab8b735
Robin <rbn_chou@yahoo.com>
2019-04-24 16:28:51 -0700

    fixed segues????test plox

---
+++ LoginApp/ViewController4.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController4.swift
+//  LoginApp
+//
+//  Created by Robin Chou on 4/24/19.
+//  Copyright © 2019 Robin Chou. All rights reserved.
+//
+
+import UIKit
+import FirebaseUI
+class ViewController4: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
